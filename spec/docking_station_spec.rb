b0b0fd81860893d551bbed0339596bb8a352d286
George Sykes <georgesykes86@gmail.com>
2018-04-17 16:45:11 +0100

    Adding docking functionality

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,6 +9,21 @@ describe DockingStation do
     expect(bike).to be_an_instance_of(Bike)
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  context 'when bike is docked' do
+    it 'returns bike that has been docked' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'shows docked bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+  end
+
   context 'when bike is working' do
     it 'returns a working bike' do
       bike = subject.release_bike
